acf353e160fa41e8963e016622b332896f2e3bcb
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-24 00:57:03 +0300

    feat: add Header component styles

--- src/components/HeaderSocialMedia/HeaderSocialMedia.jsx
+++ src/components/HeaderSocialMedia/HeaderSocialMedia.jsx
@@ -5,7 +5,7 @@ const HeaderSocialMedia = () => {
   return (
     <div className="header__socialmedia">
       <a href="https://linkedin.com" rel="noreferrer" target="_blank"><BsLinkedin /></a>
-      <a href="http://github.com" rel="noreferrer" target="_blank"><BsGithub /></a>
+      <a href="https://github.com" rel="noreferrer" target="_blank"><BsGithub /></a>
       <a href="https://twitter.com" rel="noreferrer" target="_blank"><BsTwitter /></a>
     </div>
   );
