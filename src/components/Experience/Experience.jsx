28701e2865d538415d2410f6f58dd091d63e2e99
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-25 23:56:14 +0300

    feat: add Experience component styles

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -11,67 +11,89 @@ const Experience = () => {
 
       <div className="container experience__container">
         <div className="experience__frontend">
-          <h3>Frontend Development</h3>
+          <h3 className="experience__container--heading">Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>CSS</h4>
-              <small className="text-light">Experienced</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>JavaScript</h4>
-              <small className="text-light">Intermediate</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>Bootstrap</h4>
-              <small className="text-light">Intermediate</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>Tailwind</h4>
-              <small className="text-light">Beginner</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>React</h4>
-              <small className="text-light">Intermediate</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
           </div>
         </div>
         <div className="experience__backend">
-          <h3>Backend Development</h3>
+          <h3 className="experience__container--heading">Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>Node JS</h4>
-              <small className="text-light">Beginner</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Beginner</small>
+              </div>  
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>Express JS</h4>
-              <small className="text-light">Beginner</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>Express JS</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>MongoDB</h4>
-              <small className="text-light">Beginner</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>Firebase</h4>
-              <small className="text-light">Beginner</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>Firebase</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
-              <AiOutlineCheckCircle />
-              <h4>MySQL</h4>
-              <small className="text-light">Beginner</small>
+              <AiOutlineCheckCircle className="experience__details--icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
           </div>
         </div>
