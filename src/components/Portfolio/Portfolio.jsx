4b1e211b0481f73b868cb1591540d1e495f27bba
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-29 16:47:10 +0300

    feat: add Portfolio component styles

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -18,36 +18,44 @@ const Portfolio = () => {
           <div className="portfolio__item--image">
             <img src={ReactRealEstateMarketplace} alt="React Real Estate Marketplace" />
           </div>
-          <h3>Real Estate Marketplace App</h3>
-          <a className="btn" href="https://github.com/VladimirVolvakov/real-estate-marketplace" target="_blank" rel="noreferrer">GitHub</a>
-          <a className="btn btn-primary" href="https://real-estate-market-pi.vercel.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          <h3 className="portfolio__item--heading">Real Estate Marketplace App</h3>
+          <div className="portfolio__item--call-to-action">
+            <a className="btn" href="https://github.com/VladimirVolvakov/real-estate-marketplace" target="_blank" rel="noreferrer">GitHub</a>
+            <a className="btn btn-primary" href="https://real-estate-market-pi.vercel.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          </div>
         </article>
 
         <article className="portfolio__item">
           <div className="portfolio__item--image">
             <img src={ReactMovieDatabaseImage} alt="React Movie Database" />
           </div>
-          <h3>Movie Database App</h3>
-          <a className="btn" href="https://github.com/VladimirVolvakov/react-movie-database" target="_blank" rel="noreferrer">GitHub</a>
-          <a className="btn btn-primary" href="https://melodious-torrone-14d58e.netlify.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          <h3 className="portfolio__item--heading">Movie Database App</h3>
+          <div className="portfolio__item--call-to-action">
+            <a className="btn" href="https://github.com/VladimirVolvakov/react-movie-database" target="_blank" rel="noreferrer">GitHub</a>
+            <a className="btn btn-primary" href="https://melodious-torrone-14d58e.netlify.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          </div>
         </article>
 
         <article className="portfolio__item">
           <div className="portfolio__item--image">
             <img src={PhotoBootcamp} alt="Photo Bootcamp" />
           </div>
-          <h3>Photo Bootcamp Website</h3>
-          <a className="btn" href="https://github.com/VladimirVolvakov/photo-bootcamp" target="_blank" rel="noreferrer">GitHub</a>
-          <a className="btn btn-primary" href="https://photo-bootcamp.vercel.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          <h3 className="portfolio__item--heading">Photo Bootcamp Website</h3>
+          <div className="portfolio__item--call-to-action">
+            <a className="btn" href="https://github.com/VladimirVolvakov/photo-bootcamp" target="_blank" rel="noreferrer">GitHub</a>
+            <a className="btn btn-primary" href="https://photo-bootcamp.vercel.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          </div>
         </article>
 
         <article className="portfolio__item">
           <div className="portfolio__item--image">
             <img src={ReactWeatherApp} alt="React Weather Application" />
           </div>
-          <h3>React Weather App</h3>
-          <a className="btn" href="https://github.com/VladimirVolvakov/weather-app" target="_blank" rel="noreferrer">GitHub</a>
-          <a className="btn btn-primary" href="https://weather-app-eta-five.vercel.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          <h3 className="portfolio__item--heading">React Weather App</h3>
+          <div className="portfolio__item--call-to-action">
+            <a className="btn" href="https://github.com/VladimirVolvakov/weather-app" target="_blank" rel="noreferrer">GitHub</a>
+            <a className="btn btn-primary" href="https://weather-app-eta-five.vercel.app/" target="_blank" rel="noreferrer">Watch Live</a>
+          </div>
         </article>
       </div>
     </section>
