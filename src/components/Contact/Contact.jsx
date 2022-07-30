a30d8f663cb5beb0072c65b4a71eb93432786030
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-30 20:44:51 +0300

    feat: add Contact component styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,41 +13,41 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__options--item">
-            <AiOutlineMobile />
+            <AiOutlineMobile className="contact__options--item-icon" />
             <h4>Mobile phone</h4>
             <h5>+38-071-901-77-55</h5>
-            <a href="tel:+380719017755">Call</a>
+            <a className="contact__options--item-link" href="tel:+380719017755">Call</a>
           </article>
           <article className="contact__options--item">
-            <AiOutlineMail />
+            <AiOutlineMail className="contact__options--item-icon" />
             <h4>Email</h4>
             <h5>dr.volvakov@gmail.com</h5>
-            <a href="mailto:dr.volvakov@gmail.com" target="_blank">Send a message</a>
+            <a className="contact__options--item-link" href="mailto:dr.volvakov@gmail.com" target="_blank">Send a message</a>
           </article>
           <article className="contact__options--item">
-            <AiOutlineWhatsApp />
+            <AiOutlineWhatsApp className="contact__options--item-icon" />
             <h4>WhatsApp</h4>
             <h5>+38-071-901-77-55</h5>
-            <a href="https://wa.me/+380719017755?text=urlencodedtext" target="_blank">Send a message</a>
+            <a className="contact__options--item-link" href="https://wa.me/+380719017755?text=urlencodedtext" target="_blank">Send a message</a>
           </article>
           <article className="contact__options--item">
-            <FaViber />
+            <FaViber className="contact__options--item-icon" />
             <h4>Viber</h4>
             <h5>+38-050-901-77-55</h5>
-            <a href="https://msng.link/o/?380719017755=vi" target="_blank">Send a message</a>
+            <a className="contact__options--item-link" href="https://msng.link/o/?380719017755=vi" target="_blank">Send a message</a>
           </article>
           <article className="contact__options--item">
-            <FaTelegramPlane />
+            <FaTelegramPlane className="contact__options--item-icon" />
             <h4>Telegram</h4>
             <h5>+38-050-901-77-55</h5>
-            <a href="https://t.me/VolodymyrVolvakov" target="_blank">Send a message</a>
+            <a className="contact__options--item-link" href="https://t.me/VolodymyrVolvakov" target="_blank">Send a message</a>
           </article>
         </div>
         <form>
           <input type="text" name="name" id="name" placeholder="Please enter your name" required />
           <input type="email" name="email" id="email" placeholder="Please enter your email" required />
           <textarea name="message" id="message" rows="5" placeholder="Please type your message here" required></textarea>
-          <button className="btn btn-primary" type="submit">Send Message</button>
+          <button className="btn btn-primary contact__form--button" type="submit">Send Message</button>
         </form>
       </div>
     </section>
