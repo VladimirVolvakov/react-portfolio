40fd642ae75367f56e273e9fa4d4a6ee9769ddfe
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-31 22:23:42 +0300

    feat: add Footer component styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,20 +6,20 @@ import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">Volodymyr Volvakov</a>
+      <a href="#" className="footer__logo footer__link-item">Web Developer Volodymyr Volvakov</a>
 
       <ul className="footer__links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About Me</a></li>
-        <li><a href="#experience">My Experience</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact Me</a></li>
+        <li><a href="#" className="footer__link-item">Home</a></li>
+        <li><a href="#about" className="footer__link-item">About Me</a></li>
+        <li><a href="#experience" className="footer__link-item">My Experience</a></li>
+        <li><a href="#portfolio" className="footer__link-item">Portfolio</a></li>
+        <li><a href="#contact" className="footer__link-item">Contact Me</a></li>
       </ul>
 
       <div className="footer__socialmedia">
-        <a href="https://www.linkedin.com/in/vladimir-volvakov-7243411a3/" rel="noreferrer" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com/VladimirVolvakov" rel="noreferrer" target="_blank"><BsGithub /></a>
-        <a href="https://twitter.com/VladVolvakov" rel="noreferrer" target="_blank"><BsTwitter /></a>
+        <a href="https://www.linkedin.com/in/vladimir-volvakov-7243411a3/" className="footer__link-item footer__sm" rel="noreferrer" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/VladimirVolvakov" className="footer__link-item footer__sm" rel="noreferrer" target="_blank"><BsGithub /></a>
+        <a href="https://twitter.com/VladVolvakov" className="footer__link-item footer__sm" rel="noreferrer" target="_blank"><BsTwitter /></a>
       </div>
 
       <div className="footer__copyright">
