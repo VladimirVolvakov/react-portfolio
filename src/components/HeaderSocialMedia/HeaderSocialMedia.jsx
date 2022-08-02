6ffa02e433fa9368f1579e0ae3d92064625c97a6
VladimirVolvakov <dr.volvakov@gmail.com>
2022-08-02 12:13:51 +0300

    fix: change non-working links in header

--- src/components/HeaderSocialMedia/HeaderSocialMedia.jsx
+++ src/components/HeaderSocialMedia/HeaderSocialMedia.jsx
@@ -4,9 +4,9 @@ import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
 const HeaderSocialMedia = () => {
   return (
     <div className="header__socialmedia">
-      <a href="https://linkedin.com" rel="noreferrer" target="_blank"><BsLinkedin /></a>
-      <a href="https://github.com" rel="noreferrer" target="_blank"><BsGithub /></a>
-      <a href="https://twitter.com" rel="noreferrer" target="_blank"><BsTwitter /></a>
+      <a href="https://www.linkedin.com/in/vladimir-volvakov-7243411a3/" rel="noreferrer" target="_blank"><BsLinkedin /></a>
+      <a href="https://github.com/VladimirVolvakov" rel="noreferrer" target="_blank"><BsGithub /></a>
+      <a href="https://twitter.com/VladVolvakov" rel="noreferrer" target="_blank"><BsTwitter /></a>
     </div>
   );
 };
