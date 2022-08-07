5fa887254ee32d5efdd294389db1d1ccdf914dc9
VladimirVolvakov <dr.volvakov@gmail.com>
2022-08-08 00:49:18 +0300

    fix: use createRoot instead of ReactDOM.render in index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-document.querySelector('#root'));
+  </React.StrictMode>
+);
